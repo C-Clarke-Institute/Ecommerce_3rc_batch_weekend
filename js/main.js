8048be70b6669374ba399a2c811e9b417ca899b7
DinalSanjula <dinalsanjulanw@gmail.com>
2026-07-11 10:25:14 +0530

    intialized

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Mobile Menu
+
+    const menuBtn = document.getElementById("menuBtn");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    if (menuBtn && mobileMenu) {
+
+        menuBtn.addEventListener("click", () => {
+
+            mobileMenu.classList.toggle("active");
+
+        });
+
+    }
+
+});
